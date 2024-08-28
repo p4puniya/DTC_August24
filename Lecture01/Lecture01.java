3dfbef8b2a02463ef8a99be297f95762dd90c8b3
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-08-28 16:14:48 +0530

    Pattern Printing

--- Lecture01/Lecture01.java
+++ Lecture01/Lecture01.java
@@ -1,5 +1,29 @@
 public class Lecture01{
     public static void main(String[] args){
-        System.out.print("hello");
+        int a=2;
+        a<<=7;
+        //a=a>>3;
+        // a+=2;
+        // int c=-10;
+        // float d= 1.2f;
+        // char f= 'l';
+        // double e= 8.6d;
+        // boolean g= true;
+        // long h= 1000;
+        // // a++;
+        // System.out.println(a--);
+        // if(a==10 || b==12)
+        // System.out.println(a);
+        // else
+        //loops
+        // for(int i=1; i<=10;i++)
+        //     System.out.println(i);
+            
+        // System.out.println(a);
+        
+
+        // int i=1;
+        // while(i<=10)
+        //     System.out.println(i++);
     }
 }
